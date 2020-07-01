8aa13e92853265415bc6a92e282bcdd963042d55
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-01 16:28:30 +0530

    Integrated backend api to create a new event

--- src/components/ProductAdmin.js
+++ src/components/ProductAdmin.js
@@ -1,164 +1,138 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
-import Product from "./Product";
+import { v1 as uuidv1 } from 'uuid';
 
 const config = require("../config.json");
 
 export default class ProductAdmin extends Component {
-  state = {
-    newproduct: {
-      productname: "",
-      id: "",
-    },
-    products: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      formInput: {
+        name: "",
+        description: "",
+        status: "",
+        startTime: "",
+        stopTime: "",
+      },
+    };
+    this.baseState = this.state;
+  }
 
-  handleAddProduct = async (id, event) => {
+  createRequestBody = () => {
+    return {
+      ...this.state.formInput,
+      start_time: this.state.formInput.startTime,
+      stop_time: this.state.formInput.stopTime,
+      username: this.props.auth.username,
+      eventId: uuidv1()
+    }
+  }
+
+  handleAddProduct = async (event) => {
     event.preventDefault();
-    // add call to AWS API Gateway add product endpoint here
     try {
-      const params = {
-        id,
-        productname: this.state.newproduct.productname,
-      };
-      await axios.post(`${config.api.invokeUrl}/products/${id}`, params);
-      this.setState({
-        products: [...this.state.products, this.state.newproduct],
-      });
-      this.setState({ newproduct: { productname: "", id: "" } });
+      const newEvent = this.createRequestBody();
+      const resp = await axios.post(`${config.api.invokeUrl}/events/`, newEvent);
+      alert(`Created Successfully! Event ID: ${newEvent.eventId}`);
+      this.setState(this.baseState)
     } catch (err) {
+      alert("New Event Creation Unsuccessful! Please Try Again");
       console.log(`An error has occurred: ${err}`);
     }
   };
 
-  handleUpdateProduct = async (id, name) => {
-    // add call to AWS API Gateway update product endpoint here
-    try {
-      const params = {
-        id,
-        productname: name,
-      };
-      await axios.patch(`${config.api.invokeUrl}/products/${id}`, params);
-      const productToUpdate = [...this.state.products].find(
-        (product) => product.id === id
-      );
-      const updatedProducts = [...this.state.products].filter(
-        (product) => product.id !== id
-      );
-      productToUpdate.productname = name;
-      updatedProducts.push(productToUpdate);
-      this.setState({ products: updatedProducts });
-    } catch (err) {
-      console.log(`Error updating product: ${err}`);
-    }
-  };
-
-  handleDeleteProduct = async (id, event) => {
-    event.preventDefault();
-    // add call to AWS API Gateway delete product endpoint here
-    try {
-      await axios.delete(`${config.api.invokeUrl}/products/${id}`);
-      const updatedProducts = [...this.state.products].filter(
-        (product) => product.id !== id
-      );
-      this.setState({ products: updatedProducts });
-    } catch (err) {
-      console.log(`Unable to delete product: ${err}`);
-    }
-  };
-
-  fetchProducts = async () => {
-    // add call to AWS API Gateway to fetch products here
-    // then set them in state
-    try {
-      const res = await axios.get(`${config.api.invokeUrl}/products`);
-      const products = res.data;
-      this.setState({ products });
-    } catch (err) {
-      console.log(`An error has occurred: ${err}`);
-    }
-  };
-
-  onAddProductNameChange = (event) =>
+  onFornInputChange = (event) => {
+    const changedInput = { ...this.state.formInput };
+    changedInput[event.target.id] = event.target.value;
     this.setState({
-      newproduct: { ...this.state.newproduct, productname: event.target.value },
+      formInput: changedInput
     });
-
-  onAddProductIdChange = (event) =>
-    this.setState({
-      newproduct: { ...this.state.newproduct, id: event.target.value },
-    });
-
-  componentDidMount = () => {
-    this.fetchProducts();
-  };
+  }
 
   render() {
     return (
-      <>
-        <section className="section">
-          <div className="container">
-            <h1>Product Admin</h1>
-            <p className="subtitle is-5">
-              Add and remove products using the form below:
+      <section className="section">
+        <div className="container">
+          <h1>Add New Event</h1>
+          <p className="subtitle is-5">
+            Fill in the details to add new event
             </p>
-            <br />
-            <div className="columns">
-              <div className="column is-one-third">
-                <form
-                  onSubmit={(event) =>
-                    this.handleAddProduct(this.state.newproduct.id, event)}
-                >
-                  <div className="field has-addons">
-                    <div className="control">
-                      <input
-                        className="input is-medium"
-                        type="text"
-                        placeholder="Enter name"
-                        value={this.state.newproduct.productname}
-                        onChange={this.onAddProductNameChange}
-                      />
-                    </div>
-                    <div className="control">
-                      <input
-                        className="input is-medium"
-                        type="text"
-                        placeholder="Enter id"
-                        value={this.state.newproduct.id}
-                        onChange={this.onAddProductIdChange}
-                      />
-                    </div>
-                    <div className="control">
-                      <button
-                        type="submit"
-                        className="button is-primary is-medium"
-                      >
-                        Add product
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <div className="column is-two-thirds">
-                <div className="tile is-ancestor">
-                  <div className="tile is-4 is-parent  is-vertical">
-                    {this.state.products.map((product, index) => (
-                      <Product
-                        isAdmin
-                        handleUpdateProduct={this.handleUpdateProduct}
-                        handleDeleteProduct={this.handleDeleteProduct}
-                        name={product.productname}
-                        id={product.id}
-                        key={product.id}
-                      />
-                    ))}
-                  </div>
+          <br />
+          <div className="columns">
+            <div className="column is-one-third">
+              <form
+                onSubmit={(event) =>
+                  this.handleAddProduct(event)}
+              >
+                <div className="control">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="name"
+                    placeholder="Enter Event Name"
+                    value={this.state.formInput.name}
+                    onChange={this.onFornInputChange}
+                    required
+                  />
                 </div>
-              </div>
+                <br></br>
+                <div className="control">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="description"
+                    placeholder="Enter Event Description"
+                    value={this.state.formInput.description}
+                    onChange={this.onFornInputChange}
+                  />
+                </div>
+                <br></br>
+                <div className="control">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="status"
+                    placeholder="Enter Event Status"
+                    value={this.state.formInput.status}
+                    onChange={this.onFornInputChange}
+                    required
+                  />
+                </div>
+                <br></br>
+                <div className="control">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="startTime"
+                    placeholder="Enter Event Start Time"
+                    value={this.state.formInput.startTime}
+                    onChange={this.onFornInputChange}
+                  />
+                </div>
+                <br></br>
+                <div className="control">
+                  <input
+                    className="input is-medium"
+                    type="text"
+                    id="stopTime"
+                    placeholder="Enter Event Stop Time"
+                    value={this.state.formInput.stopTime}
+                    onChange={this.onFornInputChange}
+                  />
+                </div>
+                <br></br>
+                <button
+                  type="submit"
+                  className="button is-primary is-medium"
+                >
+                  Create Event
+                </button>
+              </form>
             </div>
           </div>
-        </section>
-      </>
+        </div>
+      </section>
     );
   }
 }
